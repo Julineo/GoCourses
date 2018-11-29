114ff27477668e40aa1e29fe3d46c83a29430a6f
Julineo <advoretskiy80@gmail.com>
2018-11-28 20:52:49 -0800

    added check for first run

--- Go/Week1/Task1/hw1_tree/main.go
+++ Go/Week1/Task1/hw1_tree/main.go
@@ -22,57 +22,67 @@ func main() {
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	if err := tree(path, "", !printFiles); err != nil {
-		return err
-	}
-	return nil
-}
 
-func tree(root, indent string, offFile bool) error {
-	fi, err := os.Stat(root)
-	if err != nil {
-		return fmt.Errorf("could not stat %s: %v", root, err)
-	}
-	fmt.Println(fi.Name())
-	if !fi.IsDir() {
+	flag := false
+
+	var tree func(root, indent string, offFile bool) error
+
+	tree = func(root, indent string, offFile bool) error {
+		fi, err := os.Stat(root)
+		if err != nil {
+			return fmt.Errorf("could not stat %s: %v", root, err)
+		}
+		if fi.IsDir() {
+			if flag {
+				fmt.Println(fi.Name())
+			}
+		} else {
+			fmt.Println(fi.Name())
+			return nil
+		}
+		flag = true
+
+		fis, err := ioutil.ReadDir(root)
+		if err != nil {
+			return fmt.Errorf("could not read dir %s: %v", root, err)
+		}
+
+		var names []string
+		for _, fi := range fis {
+			if fi.Name()[0] != '.' {
+
+				// check if file -f continue
+				fi, err := os.Stat(filepath.Join(root, fi.Name()))
+				if err != nil {
+					fmt.Errorf("could not stat %s: %v", filepath.Join(root, fi.Name()), err)
+				}
+				if !fi.IsDir() && offFile {
+					continue
+				}
+
+				names = append(names, fi.Name())
+			}
+		}
+
+		for i, name := range names {
+			add := "│	"
+			if i == len(names)-1 {
+				fmt.Printf(indent + "└───")
+				add = "	"
+			} else {
+				fmt.Printf(indent + "├───")
+			}
+
+			if err := tree(filepath.Join(root, name), indent+add, offFile); err != nil {
+				return err
+			}
+		}
+
 		return nil
 	}
 
-	fis, err := ioutil.ReadDir(root)
-	if err != nil {
-		return fmt.Errorf("could not read dir %s: %v", root, err)
+	if err := tree(path, "", !printFiles); err != nil {
+		return err
 	}
-
-	var names []string
-	for _, fi := range fis {
-		if fi.Name()[0] != '.' {
-
-			// check if file -f continue
-			fi, err := os.Stat(filepath.Join(root, fi.Name()))
-			if err != nil {
-				fmt.Errorf("could not stat %s: %v", filepath.Join(root, fi.Name()), err)
-			}
-			if !fi.IsDir() && offFile {
-				continue
-			}
-
-			names = append(names, fi.Name())
-		}
-	}
-
-	for i, name := range names {
-		add := "│  "
-		if i == len(names)-1 {
-			fmt.Printf(indent + "└───")
-			add = "   "
-		} else {
-			fmt.Printf(indent + "├───")
-		}
-
-		if err := tree(filepath.Join(root, name), indent+add, offFile); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
