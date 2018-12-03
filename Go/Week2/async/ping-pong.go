52f5bc731b07f0ce72c9a8e531d35aa6f5366a5d
Julineo <advoretskiy80@gmail.com>
2018-12-02 20:45:29 -0800

    added concurent ping pong example

---
+++ Go/Week2/async/ping-pong.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var ball int
+	table := make(chan int)
+	go player(table)
+	go player(table)
+
+	table <- ball
+	time.Sleep(1 * time.Second)
+	<-table
+}
+
+func player(table chan int) {
+	for {
+		ball := <-table
+		ball++
+		fmt.Println(ball)
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
+	}
+}
