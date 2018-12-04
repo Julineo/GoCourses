1f98343c184889e24b6eb60c82a25ee998e82642
Julineo <advoretskiy80@gmail.com>
2018-12-03 17:00:45 -0800

    worked on concurency

--- Go/Week2/Task2/hw2_signer/signer.go
+++ Go/Week2/Task2/hw2_signer/signer.go
@@ -1,3 +1,34 @@
 package main
 
 // сюда писать код
+
+import (
+	"fmt"
+)
+
+func ExecutePipeline(fs ...job) {
+
+	ch1 := make(chan interface{})
+	ch2 := make(chan interface{})
+
+	for _, f := range fs {
+		ch1, ch2 = ch2, ch1
+		go f(ch1, ch2)
+	//	<-ch2
+	}
+}
+
+func SingleHash(in, out chan interface{}) {
+	fmt.Println(1)
+	out <- 1
+}
+
+func MultiHash(in, out chan interface{}) {
+	fmt.Println(2)
+	out <- 2
+}
+
+func CombineResults(in, out chan interface{}) {
+	fmt.Println(3)
+	out <- 3
+}
