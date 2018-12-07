b30457fc5cf3c666eda57adea994f23818dbd485
Julineo <advoretskiy80@gmail.com>
2018-12-06 17:01:12 -0800

    worked on multi hash siger

--- Go/Week2/Task2/hw2_signer/signer.go
+++ Go/Week2/Task2/hw2_signer/signer.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"sort"
 	"sync"
+	"strings"
+	"strconv"
 )
 
 func ExecutePipeline(fs ...job) {
@@ -71,32 +73,69 @@ func singleHashWorker(dat interface{}, out chan interface{}, wg *sync.WaitGroup,
 func MultiHash(in, out chan interface{}) {
 
 	wg := &sync.WaitGroup{}
-	//	mux := &sync.Mutex{}
 
 	for dat := range in {
 		wg.Add(1)
-		go multiHashWorker(dat, out, wg /*, mux*/)
+		go multiHashWorker(dat, out, wg)
 	}
 
 	wg.Wait()
 }
 
-func multiHashWorker(dat interface{}, out chan interface{}, wg *sync.WaitGroup /*, mux *sync.Mutex*/) {
+/*
+func multiHashWorker(dat interface{}, out chan interface{}, wg *sync.WaitGroup) {
 	data := dat.(string)
+	mux := &sync.Mutex{}
+	wg2 := &sync.WaitGroup{}
 
 	ths := []string{"0", "1", "2", "3", "4", "5"}
 	step := ""
 	for _, th := range ths {
-		tmpChan := make(chan string)
+		wg2.Add(1)
+		th := th
 		go func() {
-			tmpChan <- DataSignerCrc32(th + data)
-		}()
-		fmt.Println("data: ", data, "th: ", th, "step: ", step)
-		step += <-tmpChan
-	}
-	out <- step
+			crc32 := DataSignerCrc32(th + data)
 
+			mux.Lock()
+			step += crc32
+			fmt.Println("data: ", data, "th: ", th, "ss: ", step)
+			mux.Unlock()
+
+			wg2.Done()
+		}()
+	}
+	wg2.Wait()
+	out <- step
 	wg.Done()
+}
+*/
+
+func multiHashWorker(in interface{}, out chan interface{}, wg *sync.WaitGroup) {
+	Th := 6
+	defer wg.Done()
+	mu := &sync.Mutex{}
+	wgCrc32 := &sync.WaitGroup{}
+
+	concatArray := make([]string, Th)
+//	step := ""
+	for i := 0; i < Th; i++ {
+		wgCrc32.Add(1)
+		data := strconv.Itoa(i) + in.(string)
+		go func(data string, index int, wg *sync.WaitGroup, mu *sync.Mutex) {
+			defer wg.Done()
+			data = DataSignerCrc32(data)
+			mu.Lock()
+			concatArray[index] = data
+			//step += data
+			fmt.Printf("%s MultiHash: crc32(th+step1)) %d %s\n", in, index, data)
+			mu.Unlock()
+		}(data, i, wgCrc32, mu)
+	}
+	wgCrc32.Wait()
+	result := strings.Join(concatArray, "")
+//	result := step
+	fmt.Printf("%s MultiHash result: %s\n", in, result)
+	out <- result
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -111,6 +150,5 @@ func CombineResults(in, out chan interface{}) {
 	for i := 1; i < len(ar); i++ {
 		res += "_" + ar[i]
 	}
-	fmt.Println(res)
 	out <- res
 }
